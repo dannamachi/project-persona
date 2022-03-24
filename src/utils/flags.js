0ed11331b540772c8afd8b185925be53ea91f51d
dannamachi <serenita15@gmail.com>
2022-03-24 22:23:14 +1100

    add flag util module

---
+++ src/utils/flags.js
@@ -0,0 +1,104 @@
+// const SETTER_FLAGS = ['flag', 'score', 'value', 'inc', 'dec']
+// const REQ_FLAGS = ['flag', 'score', 'value', 'more', 'less', 'diff']
+
+const isOfFlagType = (flag, ft) => {
+    switch (ft) {
+        case "flag": return flag.type == 'flag';
+        case "score": return flag.type in ['score', 'inc', 'dec', 'more', 'less'];
+        case "value": return flag.type in ['value', 'diff']
+    }
+    return false
+}
+
+const updateScoreFlag = (ofl, nfl, ft) => {
+    switch (ft) {
+        case "score":
+            ofl.score = nfl.score;
+            break;
+        case "inc":
+            ofl.score += nfl.score;
+            break;
+        case "dec":
+            ofl.score -= nfl.score;
+            break;
+    }
+    return ofl;
+}
+
+const updateValueFlag = (ofl, nfl) => {
+    ofl.value = nfl.value
+    return ofl;
+}
+
+const setFlag = (flags, flagToSet) => {
+    var hasFlag = false
+    var fl;
+    for (var i=0; i<flags.length; i++) {
+        fl = flags[i];
+        if (isEqualFlag(fl, flagToSet)) {
+            if (isOfFlagType(flagToSet, "score")) flags[i] = updateScoreFlag(fl, flagToSet, flagToSet.type)
+            else if (isOfFlagType(flagToSet, "value")) flags[i] = updateValueFlag(fl, flagToSet)
+            else if (isOfFlagType(flagToSet, "flag")) hasFlag = true
+        }
+    }
+    if (isOfFlagType(flagToSet, "flag") && !hasFlag) {
+        flags.push(flagToSet)
+    }
+    return flags;
+}
+
+const checkScoreFlag = (current, req, ft) => {
+    switch (ft) {
+        case "score":
+            return current.score == req.score;
+        case "more":
+            return current.score >= req.score;
+        case "less":
+            return current.score < req.score;
+    }
+    return false;
+}
+
+const checkValueFlag = (current, req, ft) => {
+    switch (ft) {
+        case "value":
+            return current.value == req.value;
+        case "diff":
+            return current.value != req.value;
+    }
+    return false;
+}
+
+const hasFlag = (flags, reqFlag) => {
+    var fl;
+    for (var i=0; i<flags.length; i++) {
+        fl = flags[i];
+        if (isEqualFlag(fl, reqFlag)) {
+            if (isOfFlagType(reqFlag, "score")) {
+                if (checkScoreFlag(fl, reqFlag, reqFlag.type)) return true
+            } 
+            else if (isOfFlagType(reqFlag, "value")) {
+                if (checkValueFlag(fl, reqFlag, reqFlag.type)) return true
+            }
+            else if (isOfFlagType(reqFlag, "flag")) return true
+        }
+    }
+    return false
+}
+
+const hasFlags = (flags, requiredList) => {
+    for (var fl of requiredList) {
+        if (!hasFlag(flags, fl)) return false
+    }
+    return true
+}
+
+const isEqualFlag = (flag1, flag2) => {
+    if (flag1.name != flag2.name) return false
+    for (var ft of ["flag", "score", "value"]) {
+        if (isOfFlagType(flag1, ft) && isOfFlagType(flag2, ft)) return true;
+    }
+    return false;
+}
+
+export { setFlag, isEqualFlag, hasFlag, hasFlags };
